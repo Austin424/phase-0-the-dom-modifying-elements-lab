6ca857ba052a88fd39da41280734ce8c14af38cc
Austin Rivera <austin.rivera.24@aol.com>
2023-12-19 02:27:47 -0500

    Create and Insert DOM nodes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+var mainElement = document.getElementById('main');
+
+if (mainElement) {
+  mainElement.parentNode.removeChild(mainElement);
+}
+
+var newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+document.body.appendChild(newHeader);
+
+var yourName = 'Austin Rivera ';
+
+newHeader.innerHTML = yourName + 'is the champion';
+
+document.body.appendChild(newHeader);
